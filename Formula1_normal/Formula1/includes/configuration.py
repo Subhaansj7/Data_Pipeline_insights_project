57b425ee43f5febbc6d1d9f1b056cfc42138281a
Subhaan Jirait <101044609+Subhaansj7@users.noreply.github.com>
2024-06-28 16:31:00 +0530

    Delete Formula1_normal directory

--- Formula1_normal/Formula1/includes/configuration.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-raw_folder_path='/mnt/formula1dlil/raw'
-processed_folder_path='mnt/formula1dlil/processed'
-presentation_folder_path='mnt/formula1dlil/presentation'
-
-# COMMAND ----------
-
